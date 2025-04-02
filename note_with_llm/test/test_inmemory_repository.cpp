2c66be7d9f7c86f71aeb05aa629ee2a5c45754e4
sangwon lee <leeh8911@gmail.com>
2025-04-03 05:58:09 +0900

    [FEAT] Add testing library and test code

---
+++ note_with_llm/test/test_inmemory_repository.cpp
@@ -0,0 +1,69 @@
+#include <doctest/doctest.h>
+
+#include "repository/inmemory_repository.hpp"
+
+TEST_CASE("InMemoryRepository")
+{
+    banchoo::repository::InMemoryRepository repo;
+
+    SUBCASE("createNote")
+    {
+        banchoo::note::Note n;
+        n.content = "Hello, World!";
+        auto id = repo.createNote(n);
+        CHECK(id == 1);
+    }
+
+    SUBCASE("getNote")
+    {
+        banchoo::note::Note n;
+        n.content = "Hello, World!";
+        auto id = repo.createNote(n);
+
+        auto result = repo.getNote(id);
+        REQUIRE(result.has_value());
+        CHECK(result->content == "Hello, World!");
+    }
+
+    SUBCASE("getAllNotes")
+    {
+        banchoo::note::Note n1;
+        n1.content = "Hello, World!";
+        auto id1 = repo.createNote(n1);
+
+        banchoo::note::Note n2;
+        n2.content = "Hello, C++!";
+        auto id2 = repo.createNote(n2);
+
+        auto all = repo.getAllNotes();
+        REQUIRE(all.size() == 2);
+        CHECK(all[0].content == "Hello, World!");
+        CHECK(all[1].content == "Hello, C++!");
+    }
+
+    SUBCASE("updateNote")
+    {
+        banchoo::note::Note n;
+        n.content = "Hello, World!";
+        auto id = repo.createNote(n);
+
+        n.content = "Hello, C++!";
+        CHECK(repo.updateNote(n));
+
+        auto result = repo.getNote(id);
+        REQUIRE(result.has_value());
+        CHECK(result->content == "Hello, C++!");
+    }
+
+    SUBCASE("deleteNote")
+    {
+        banchoo::note::Note n;
+        n.content = "Hello, World!";
+        auto id = repo.createNote(n);
+
+        CHECK(repo.deleteNote(id));
+
+        auto result = repo.getNote(id);
+        CHECK(!result.has_value());
+    }
+}
